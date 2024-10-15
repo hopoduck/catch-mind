a9b05a70222546f6063ea28b130aa15dab291209
hopoduck <gksqudwh1@naver.com>
2024-10-15 22:47:15 +0900

    wip: ip 검사 제한 해제

--- backend/src/metrics/metrics.controller.ts
+++ backend/src/metrics/metrics.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Header, HttpStatus, Req, Res } from '@nestjs/common';
+import { Controller, Get, Header, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { register } from 'prom-client';
 
@@ -7,11 +7,12 @@ export class MetricsController {
   @Get()
   @Header('Content-Type', register.contentType)
   async getMetrics(@Req() req: Request, @Res() res: Response) {
-    const ip = req.headers['x-real-ip'] ?? req.ip;
-    if (!['::1', '127.0.0.1', 'localhost'].includes(ip.toString())) {
-      console.log(`you can not access metrics data. your ip : <${ip}>`);
-      return res.status(HttpStatus.UNAUTHORIZED).end();
-    }
+    // TODO: 요청 검사 후 제한
+    // const ip = req.headers['x-real-ip'] ?? req.ip;
+    // if (!['::1', '127.0.0.1', 'localhost'].includes(ip.toString())) {
+    //   console.log(`you can not access metrics data. your ip : <${ip}>`);
+    //   return res.status(HttpStatus.UNAUTHORIZED).end();
+    // }
 
     res.end(await register.metrics());
   }
