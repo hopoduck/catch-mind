b42a1bc5e03cb4abd45dbcf3a4e6a7cafe6cd7a3
hopoduck <gksqudwh1@naver.com>
2024-10-28 16:27:44 +0900

    feat: 체인지로그 추가

--- frontend/src/components/Changelog.tsx
+++ frontend/src/components/Changelog.tsx
@@ -1,8 +1,8 @@
-import { Button } from "@nextui-org/react";
+import { Badge, Button } from "@nextui-org/react";
 import { useState } from "react";
 
 // 업데이트 날짜의 unix epoch 값
-const lastChangelog = 1729761742914;
+const lastChangelog = 1730100054815;
 
 export default function Changelog() {
   const lastShowChangelog = isNaN(
@@ -12,21 +12,45 @@ export default function Changelog() {
     : Number(localStorage.getItem("lastShowChangelog"));
 
   const [show, setShow] = useState(false);
-  const [isNew, setIsNew] = useState(lastShowChangelog >= lastChangelog);
+  const [isNew, setIsNew] = useState(lastChangelog >= lastShowChangelog);
 
   return (
-    <div>
-      <Button
-        onPress={() => {
-          setShow((v) => !v);
-        }}
-      >
-        업데이트 내역
-      </Button>
+    <div className="flex w-full max-w-lg flex-col items-center justify-center gap-2">
+      {isNew ? (
+        <Badge
+          color="danger"
+          shape="circle"
+          content=""
+          showOutline={false}
+          placement="top-right"
+        >
+          <Button
+            onPress={() => {
+              setShow((v) => !v);
+              localStorage.setItem("lastShowChangelog", Date.now().toString());
+              setIsNew(false);
+            }}
+          >
+            업데이트 내역
+          </Button>
+        </Badge>
+      ) : (
+        <Button
+          onPress={() => {
+            setShow((v) => !v);
+          }}
+        >
+          업데이트 내역
+        </Button>
+      )}
       {show ? (
-        <ul>
-          <li>asdasd</li>
-        </ul>
+        <div className="m-4 w-full rounded bg-slate-300 p-4 shadow-lg">
+          <div className="text-lg">2024-10-28</div>
+          <li className="text-base">
+            문제 단어를 추가하였습니다. (1,319개 → 3,682개)
+          </li>
+          {/* <Divider /> */}
+        </div>
       ) : null}
     </div>
   );
