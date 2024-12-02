708359e8ccb18bc065d058b623973f0acc71182b
hopoduck <gksqudwh1@naver.com>
2024-12-02 23:17:23 +0900

    fix: `context.env` 수정

--- frontend/functions/_middleware.ts
+++ frontend/functions/_middleware.ts
@@ -1,35 +1,31 @@
 import proxyflare from "@flaregun-net/proxyflare-for-pages";
 import { Route } from "@flaregun-net/proxyflare-for-pages/build/types";
 
-declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace NodeJS {
-    interface ProcessEnv {
-      readonly VITE_GA_ID: string;
-      readonly VITE_CLOUDFLARE_URL: string;
-      readonly VITE_API_URL: string;
-    }
-  }
+interface EnvContext {
+  readonly VITE_GA_ID: string;
+  readonly VITE_CLOUDFLARE_URL: string;
+  readonly VITE_API_URL: string;
 }
 
-const routes: Route[] = [
-  {
-    from: {
-      pattern: `${process.env.VITE_CLOUDFLARE_URL}/api/*`,
-      alsoMatchWWWSubdomain: true,
-    },
-    to: { url: `${process.env.VITE_API_URL}/api` },
-  },
-];
-
 // `PagesFunction` is from @cloudflare/workers-types
-export const onRequest: PagesFunction[] = [
-  (context) =>
-    proxyflare({
+export const onRequest: PagesFunction<EnvContext>[] = [
+  (context) => {
+    const routes: Route[] = [
+      {
+        from: {
+          pattern: `${context.env.VITE_CLOUDFLARE_URL}/api/*`,
+          alsoMatchWWWSubdomain: true,
+        },
+        to: { url: `${context.env.VITE_API_URL}/api` },
+      },
+    ];
+
+    return proxyflare({
       config: {
         global: { debug: true },
         routes,
       },
-    })(context),
+    })(context);
+  },
   // other Pages plugins and middleware
 ];
